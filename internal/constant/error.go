21de115e03f57e58db72bec6742634cfa3adeea2
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-08-25 20:32:06 +0700

    feat: add app & error const

---
+++ internal/constant/error.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	InternalServerErrorMessage        = "currently our server is facing unexpected error, please try again later"
+	ValidationErrorMessage            = "input validation error"
+	InvalidJsonUnmarshallErrorMessage = "invalid JSON format"
+	JsonSyntaxErrorMessage            = "invalid JSON syntax"
+	InvalidJsonValueTypeErrorMessage  = "invalid value for %s"
+)
